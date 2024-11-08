915acfe7266157738b25159dfd3bfc1c082648ab
Faiq Najib <34987164+fanajib5@users.noreply.github.com>
2024-11-09 00:05:21 +0700

    Add JWT generation utility with expiration and claims handling

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,1 +1,28 @@
 package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte("your_secret_key")
+
+type Claims struct {
+	UserID uint
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(userID uint) (string, error) {
+	expirationTime := time.Now().Add(15 * time.Minute)
+	claims := &Claims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "kopelko-dating-app-backend",
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
