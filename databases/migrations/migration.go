57e72584fc2f4fe67ff77563cc45cd01340814da
Faiq Najib <34987164+fanajib5@users.noreply.github.com>
2024-11-09 21:10:42 +0700

    Update environment configuration, add PostgreSQL driver dependency, and implement database migration and seeding scripts

---
+++ databases/migrations/migration.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// Load environment variables
+	loadEnv()
+
+	// wd, err := os.ReadFile("/databases/migrations/schema.sql")
+	// if err != nil {
+	// 	log.Fatalf("Failed to get working directory: %v", err)
+	// }
+	// log.Println("Working directory:", string(wd))
+
+	// Initialize the database connection
+	db, err := initDB()
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	// Execute the migration file
+	if err := executeSQLFile(db, "databases/migrations/schema.sql"); err != nil {
+		log.Fatalf("Failed to execute migration: %v", err)
+	}
+
+	log.Println("Migration completed successfully!")
+}
+
+// Function to load environment variables from .env file
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+// Function to initialize the database connection
+func initDB() (*sql.DB, error) {
+	// Define PostgreSQL connection string using environment variables for security
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	dbTimezone := os.Getenv("DB_TIMEZONE")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=%s",
+		dbHost, dbPort, dbUser, dbPassword, dbName, dbTimezone)
+
+	// Connect to the database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+	return db, nil
+}
+
+// Function to execute SQL from file
+func executeSQLFile(db *sql.DB, filepath string) error {
+	// Read SQL file content
+	sqlBytes, err := os.ReadFile(filepath)
+	if err != nil {
+		return fmt.Errorf("failed to read file %s: %v", filepath, err)
+	}
+	sqlStatements := string(sqlBytes)
+
+	// Execute the SQL statements
+	_, err = db.Exec(sqlStatements)
+	if err != nil {
+		return fmt.Errorf("failed to execute SQL statements: %v", err)
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.12.0
+	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.29.0
 	gorm.io/driver/postgres v1.5.9
 	gorm.io/gorm v1.25.12
